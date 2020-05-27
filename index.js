d2411e78e26784421b27da2973f2e0fbebe597a1
ShaneMit <shanemit@cox.net>
2020-05-26 22:40:23 -0700

    created gets

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const express = require('express')
+const app = express()
+
 const mysql = require('mysql2')
 
 const db = mysql.createConnection({
@@ -7,3 +10,37 @@ const db = mysql.createConnection({
   password: 'rootroot',
   database: 'employee_db'
 })
+
+app.use(express.json())
+
+app.get('/departments', (req, res) => {
+  db.query(
+    'SELECT * FROM `department`',
+    function (err, results) {
+      console.log(results)
+      return res.json(results)
+    }
+  )
+})
+
+app.get('/roles', (req, res) => {
+  db.query(
+    'SELECT * FROM `role`',
+    function (err, results) {
+      console.log(results)
+      return res.json(results)
+    }
+  )
+})
+
+app.get('/employees', (req, res) => {
+  db.query(
+    'SELECT * FROM `employee`',
+    function (err, results) {
+      console.log(results)
+      return res.json(results)
+    }
+  )
+})
+
+app.listen(3000, () => console.log('http://localhost:3000'))
--- sql/seed.sql
+++ sql/seed.sql
@@ -1,3 +1,10 @@
 USE employee_db
 
-INSERT INTO 
+INSERT INTO department(name)
+VALUES ("Sales"), ("Dev Ops"), ("Engineering"), ("Marketing"), ("Design");
+
+INSERT INTO role(title, salary, department_id)
+VALUES ("Sales Agent", 1000000, 1), ("Dev Agent", 1000000, 2), ("Engineer", 1000000, 3), ("Market", 10000000, 4), ("Designer", 2000000, 5);
+
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ("Jane", "Doe", 1, 1), ("John", "Doe", 2, 1), ("Jack", "Doe", 3, 1), ("Jill", "Doe", 4, 1), ("Jeremy", "Doe", 5, 1);
