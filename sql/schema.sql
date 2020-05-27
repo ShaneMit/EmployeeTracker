8d9754df4e2cd9c9de09991f612ca589edf34e2f
ShaneMit <shanemit@cox.net>
2020-05-26 18:52:48 -0700

    created schema sql

---
+++ sql/schema.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE employee_db;
+
+USE grocery_db;
+
+CREATE TABLE department(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role( 
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+  INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL
+);
